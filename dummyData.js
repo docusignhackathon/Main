90bd08224f72b476d4aef4ff27623cc858cd53d5
Toby Hoshman <t.hoshman@gmail.com>
2019-01-28 10:10:10 -0800

    add dummy data

---
+++ dummyData.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'frozen pizza',
+    quantity: 5,
+  },
+  {
+    id: 2,
+    name: 'noosa yogurt',
+    quantity: 10,
+  },
+  {
+    id: 3,
+    name: 'wine',
+    quantity: 2,
+  },
+  {
+    id: 4,
+    name: 'iced coffee',
+    quantity: 1,
+  },
+  {
+    id: 5,
+    name: 'avocados',
+    quantity: 10,
+  },
+  {
+    id: 6,
+    name: 'pizza',
+    quantity: 1,
+  }
+];
